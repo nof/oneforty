69092bfe54c5b8895743ecaecd889f06396c6cee
nof <sakape@gmail.com>
2015-04-20 19:23:06 +0900

    ひとまずroutes決め

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, param: :name, only: [:index] do
+    resources :tweets, only: [:index]
+    resources :likes, only: [:index]
+    resources :followers, only: [:index]
+    resources :followees, only: [:index]
+    member do
+      get 'timeline'
+    end
+  end
+
+  root 'users#timeline'
+
   devise_for :users
 end

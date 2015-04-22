23f3cd8378bdd1e92a94edaa80f8317ddc3272ac
nof <sakape@gmail.com>
2015-04-22 17:21:08 +0900

    自分はフォローできないように

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,10 @@ class Relationship < ActiveRecord::Base
   belongs_to :followee, class_name: 'User'
   validates :follower_id, presence: true
   validates :followee_id, presence: true
+  validate :follow_self
+
+  private
+    def follow_self
+      errors.add(:follower_id) if (follower_id == followee_id)
+    end
 end

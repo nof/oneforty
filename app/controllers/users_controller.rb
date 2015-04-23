01e01acbd0aab3de7102468fc95cc04a5c1b8962
nof <sakape@gmail.com>
2015-04-23 13:32:28 +0900

    フォロワーを見るのに権限は不要

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:followers, :followees]
   before_action :set_user, only: [:followers, :followees]
 
   def index
